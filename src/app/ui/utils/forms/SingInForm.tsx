7c6151f6ebc2762bfa747eb130d9713a990af602
Sławek Zieliński <121642873+SlawekZet@users.noreply.github.com>
2024-09-02 20:44:38 +0200

    RWD - signup form and sigin form update

--- src/app/ui/utils/forms/SingInForm.tsx
+++ src/app/ui/utils/forms/SingInForm.tsx
@@ -23,7 +23,7 @@ export const SignInForm: React.FC<SignInFormProps> = ({
       <form onSubmit={(e) => handleSubmit(e)} className="w-3/4 mb-2">
         <div className="mb-4">
           <label
-            className="block text-gray5 dark:text-gray2 text-sm font-bold mb-2"
+            className="block text-gray5 dark:text-gray2 text-sm font-bold mb-2 mob:hidden"
             htmlFor="email"
           >
             Email
@@ -31,16 +31,16 @@ export const SignInForm: React.FC<SignInFormProps> = ({
           <input
             id="email"
             type="email"
-            className="w-full px-3 py-2 rounded-lg dark:bg-gray5"
-            placeholder="Type your email"
+            className="w-full px-3 py-2 rounded-lg dark:bg-gray5 mob:border-gray3 mob:border-[1px]"
             value={email}
+            placeholder="Type your email"
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
         <div className="mb-6">
           <label
-            className="block text-gray5 dark:text-gray2 text-sm font-bold mb-2"
+            className="block text-gray5 dark:text-gray2 text-sm font-bold mb-2 mob:hidden"
             htmlFor="password"
           >
             Password
@@ -48,22 +48,17 @@ export const SignInForm: React.FC<SignInFormProps> = ({
           <input
             id="password"
             type="password"
-            className="w-full px-3 py-2 rounded-lg dark:bg-gray5"
-            placeholder="Type password"
+            className="w-full px-3 py-2 rounded-lg dark:bg-gray5 mob:border-gray3 mob:border-[1px]"
             value={password}
+            placeholder="Type password"
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-          <div className=" text-red2 text-xs mt-2">
-            {error === 'Firebase: Error (auth/invalid-credential).'
-              ? 'Invalid credentials'
-              : null}
-          </div>
         </div>
         <div className="flex items-center justify-center">
           <Button
             // type="submit"
-            className="font-bold bg-gray2 dark:bg-gray5 px-4 py-2 rounded-lg w-[150px] text-center shadow-md"
+            className="font-bold mob:bg-gray1 mob:w-full bg-gray2 dark:bg-gray5 px-4 py-2 rounded-lg w-[150px] text-center shadow-md"
           >
             Sign In
           </Button>
